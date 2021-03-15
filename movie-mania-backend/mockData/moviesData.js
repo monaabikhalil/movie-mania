094bdff21e317981545e093ee7041b0bffabd331
monaabikhalil <mona_abikhalil@hotmail.com>
2021-03-15 12:12:38 +0200

    Added mock data.

---
+++ movie-mania-backend/mockData/moviesData.js
@@ -0,0 +1,16 @@
+export const movies = [
+    {
+        id: 0,
+        name: "Movie One",
+        genre: "Comedy",
+        description: "This is the description of Movie One",
+        pictureSource: "",
+    },
+    {
+        id: 1,
+        name: "Movie Two",
+        genre: "Action",
+        description: "This is the description of Movie Two",
+        pictureSource: "",
+    }
+];
---
+++ movie-mania-backend/mockData/usersData.js
@@ -0,0 +1,19 @@
+export const users = [
+    {
+        id: 0,
+        userName: "monaak",
+        firstName: "Mona",
+        lastName: "Abi Khalil",
+        email: "mona_abikhalil@hotmail.com",
+        ratings: [
+            {
+                movieId: 0,
+                rating: 5,
+            },
+            {
+                movieId: 1,
+                rating: 3
+            }
+        ]
+    }
+];
